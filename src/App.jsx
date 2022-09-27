c66e0b509fec2b8944463765184ce72fed1393fd
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-09-26 22:13:09 -0300

    agregue el componente card como hijo del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import {Container} from "react-bootstrap";
+// import {Container} from "react-bootstrap";
 import "../src/components/app.css"
-import Cards from "./components/Cards";
 import FormColores from "./components/FormColores";
 
 function App() {
   return (
     <>
     <FormColores></FormColores>
-    <Container>
-      <div className="row">
-        <Cards></Cards>
-      </div>
-    </Container>
     </>
   );
 }
--- src/components/FormColores.jsx
+++ src/components/FormColores.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
+import Cards from "./Cards";
 
 const FormColores = () => {
   return (
     <>
+    <Container>
       <h1 className="display-5 text-center my-5">Colores</h1>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="formBasicColor">
@@ -14,6 +16,10 @@ const FormColores = () => {
           </Button>
         </Form.Group>
       </Form>
+      <div className="row">
+        <Cards></Cards>
+      </div>
+    </Container>
     </>
   );
 };

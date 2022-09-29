43bc9d1a7f39c2846c133ea5e83adfe5adaf20f3
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-09-29 17:23:28 -0300

    modifique maquetado de las cards, dimos funcionalidad con los colores

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import Colores from "./Colores";
+import { Card, Col, Row, Button } from "react-bootstrap";
 
 const Cards = ({ listaColores, borrarColor }) => {
   return (
-        <Card>
-      {listaColores.map((colores, posicion) => (
-        <Colores
-          key={posicion}
-          nombreColor={colores}
-          borrarColor={borrarColor}
-        ></Colores>
-      ))}
+    <>
+    <Row>
+        {        
+             listaColores.map((colores, posicion)=>{return <Col sm={6} md={4} lg={3}>
+                <Card className="d-flex align-items-center border-0">
+                <Card.Body>
+              <Card.Title> {colores} </Card.Title>
+                <div className="caja" key={posicion} style={{ 'background': colores }}></div>
+            </Card.Body>
+            <Button className="my-2 btn-sm" variant="danger" onClick={()=> borrarColor(colores)}>
+              Borrar
+            </Button>
           </Card>  
+            </Col>})
+        }
+    </Row>
+    </>
   );
 };
 
--- src/components/FormColores.jsx
+++ src/components/FormColores.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button, Container, Col, Row } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Cards from "./Cards";
 
 const FormColores = () => {
@@ -7,6 +7,7 @@ const FormColores = () => {
     JSON.parse(localStorage.getItem("listaColoresKey")) || [];
   const [colores, setColores] = useState("");
   const [listaColores, setListaColores] = useState(arregloLocalStorage);
+  const [cambiarColor, setCambiarColor] = useState("");
 
   useEffect(() => {
     localStorage.setItem("listaColoresKey", JSON.stringify(listaColores));
@@ -25,31 +26,32 @@ const FormColores = () => {
     setListaColores(copiaColores);
   };
 
+  const actualizarColor = (colorIngresado) => {
+    setCambiarColor(colorIngresado);
+    setColores(colorIngresado);
+  };
 
   return (
     <>
-        <h1 className="display-5 text-center my-5">Colores</h1>
-        <Form onSubmit={arregloColores}>
-          <Form.Group className="mb-3 d-flex" controlId="formBasicColor">
-            <div className="colores mx-1" style={{ background: "blue" }}></div>
-            <Form.Control
-              type="text"
-              placeholder="Ingrese un color"
-              onChange={(e) => setColores(e.target.value)}
-              value={colores}
-            />
-            <Button variant="outline-warning mx-1" type="submit">
-              Agregar
-            </Button>
-          </Form.Group>
-        </Form>
-        <Container fluid>
-          <Row>
-            <Col md={3}>
-          <Cards listaColores={listaColores} borrarColor={borrarColor}></Cards>
-            </Col>
-          </Row>
-        </Container>
+      <h1 className="display-5 text-center my-5">Colores</h1>
+      <Form onSubmit={arregloColores}>
+        <Form.Group className="mb-3 d-flex" controlId="formBasicColor">
+          <div
+            className="caja mx-1"
+            style={{ "background": cambiarColor }}
+          ></div>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese un color"
+            onChange={(e) => actualizarColor(e.target.value)}
+            value={colores}
+          />
+          <Button variant="outline-warning mx-1" type="submit">
+            Agregar
+          </Button>
+        </Form.Group>
+      </Form>
+      <Cards listaColores={listaColores} borrarColor={borrarColor}></Cards>
     </>
   );
 };
